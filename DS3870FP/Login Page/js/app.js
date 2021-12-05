d61a68865fba2fd8c6e4c2057cf09a1a5a0fdb4e
Nicklaw42 <nalaw42@tntech.edu>
2021-12-05 16:22:24 -0600

    Beginning Functionality
    Worked on the Create Account Square on index.html.  Started implementing create account functionality.

--- DS3870FP/Login Page/js/app.js
+++ DS3870FP/Login Page/js/app.js
@@ -1,3 +1,25 @@
+$('#btnCreateUser').click(function(){
+    console.log('Create Account Clicked');
+    let strUserName = $('#txtUserName').val(); 
+    let strFirstName =$('#txtFirstName').val();
+    let strLastName =$('#txtLastName').val();
+    let strPhone =$('#txtPhone').val();
+    let strPassword =$('#txtPassword').val();
+    let strTeamCode =$('#txtTeamCode').val();
+    $.getJSON('https://www.swollenhippo.com/DS3870/DS3870/addUser.php?strUserName=' +strUserName +'&strFirstName=' +strFirstName+ '&strLastName=' +strLastName+ '&strPhone=' +strPhone+ '&strPassword=' +strPassword+ '&strTeamCode=' +strTeamCode+ '', function(data){
+        $.each(data, function (result)
+            if(result.Outcome =='Error')
+            {
+                alert('Failed To Create User');
+            }   
+            else {
+                alert('User Created');
+            }     
+        )
+    
+})
+
+
 $(document).ready(function() {
     $('#tblMaterials').DataTable({
         filter: true,
